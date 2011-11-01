38f1a2fb4ac4158a5114e2588171d7d5561d57d1
Peter R. G. Small <prgsmall@gmail.com>
2011-11-01 16:13:06 -0600

    fixed call error

--- libs/netIP.js
+++ libs/netIP.js
@@ -1,4 +1,4 @@
-/*globals exports require */
+/*globals exports require process*/
 var net = require('net');
 
 var getIPFromSocket = function (callback) {
@@ -9,65 +9,58 @@ var getIPFromSocket = function (callback) {
     });
     socket.on('error', function (e) {
         callback(e, null);
-    });                
+    });
+};               
 
-}
-var getNetworkIP = (function () {
-    try {
-        var ignoreRE = /^(127\.0\.0\.1|::1|fe80(:1)?::1(%.*)?)$/i;
-
-        var exec = require('child_process').exec;
-        var cached;    
-        var command;
-        var filterRE;
+var getNetworkIP = function (callback, bypassCache) {
+    try {        
+        var cached, command, filterRE,
+        exec = require('child_process').exec,
+        ignoreRE = /^(127\.0\.0\.1|::1|fe80(:1)?::1(%.*)?)$/i;
 
         // TODO: implement for OSs without ifconfig command
         if (process.platform === "darwin") {
-             command = 'ifconfig';
-             filterRE = /\binet\s+([^\s]+)/g;
-         } else {
-             command = 'ifconfig';
-             filterRE = /\binet\b[^:]+:\s*([^\s]+)/g;
+            command = 'ifconfig';
+            filterRE = /\binet\s+([^\s]+)/g;
+        } else {
+            command = 'ifconfig';
+            filterRE = /\binet\b[^:]+:\s*([^\s]+)/g;
         }
 
-        return function (callback, bypassCache) {
-             // get cached value
-            if (cached && !bypassCache) {
-                callback(null, cached);
-                return;
-            }
-            // system call
-            exec(command, function (error, stdout, sterr) {
-                var i, ips = [];
-                // extract IPs
-                var matches = stdout.match(filterRE);
-            
-                if (matches) {
-                    // JS has no lookbehind REs, so we need a trick
-                    for (i = 0; i < matches.length; i++) {
-                        ips.push(matches[i].replace(filterRE, '$1'));
-                    }
-
-                    // filter BS
-                    for (i = 0, l = ips.length; i < l; i++) {
-                        if (!ignoreRE.test(ips[i])) {
-                            //if (!error) {
-                                cached = ips[i];
-                            //}
-                            callback(error, ips[i]);
-                            return;
-                        }
-                    }
-                } else {
-                    getIPFromSocket(callback);
+        // get cached value
+        if (cached && !bypassCache) {
+            callback(null, cached);
+            return;
+        }
+        
+        // system call
+        exec(command, function (error, stdout, sterr) {
+            var i, l, ips = [], 
+            matches = stdout.match(filterRE);
+    
+            if (matches) {
+                // JS has no lookbehind REs, so we need a trick
+                for (i = 0; i < matches.length; i += 1) {
+                    ips.push(matches[i].replace(filterRE, '$1'));
                 }
-                // nothing found
-                callback(error, null);
-            });
-        };
+
+                // filter BS
+                for (i = 0, l = ips.length; i < l; i += 1) {
+                    if (!ignoreRE.test(ips[i])) {
+                        cached = ips[i];
+                        callback(error, ips[i]);
+                        return;
+                    }
+                }
+            } else {
+                getIPFromSocket(callback);
+            }
+            // nothing found
+            callback(error, null);
+        });
     } catch (e) {
         return getIPFromSocket(callback);
     }
-})();
+};
 
-exports.getNetworkIP=getNetworkIP;
+exports.getNetworkIP = getNetworkIP;
